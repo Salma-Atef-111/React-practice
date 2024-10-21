5e99953acbe8d27c0330a0691f250e5d3c42a726
Salma atef <salma.atef20222@gmail.com>
2024-10-22 01:39:32 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
-export default function app(){
+import { useEffect, useState } from "react";
+
+export default function App(){
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
 
   return <>
-  <h1>hiiiiiii</h1>
-  <h2>hiiiiii 22222222222</h2>
-  <h3>neeeeeeeeeeew</h3>
+    <div>
+        <h1>Hello World </h1>
+        <button onClick={getAdvice}>Get advice</button>
+        <h4>{advice}</h4>
+        <Message count={count} />
+    </div>
   </>
+}
+function Message(props) {
+  return (
+    <>
+      <p>
+        you have raed <strong>{props.count}</strong> pieces of advice
+      </p>
+    </>
+  );
 }
